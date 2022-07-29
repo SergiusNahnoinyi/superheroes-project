ee220fedf65eb75a6593c9cdc0fd07aa6c05a125
Sergius Nahnoinyi <86116594+SergiusNahnoinyi@users.noreply.github.com>
2022-07-29 13:01:20 +0300

    Creates a not found page

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import AppBar from "./components/AppBar";
 
 const HomePage = lazy(() => import("./pages/HomePage"));
 const HeroesPage = lazy(() => import("./pages/HeroesPage"));
+const NotFoundPage = lazy(() => import("./pages/NotFoundPage"));
 
 export default function App() {
   return (
@@ -15,6 +16,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/heroes" element={<HeroesPage />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </Suspense>
     </Container>
