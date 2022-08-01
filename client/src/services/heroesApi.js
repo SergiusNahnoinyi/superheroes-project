d439d1a8e51856455846edfe24385c037a721413
Sergius Nahnoinyi <86116594+SergiusNahnoinyi@users.noreply.github.com>
2022-08-01 15:55:16 +0300

    Changes path from the local server to the heroku

--- client/src/services/heroesApi.js
+++ client/src/services/heroesApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { toast } from "react-toastify";
 
-axios.defaults.baseURL = "http://localhost:4000/api";
+axios.defaults.baseURL = "https://superheroes-project-server.herokuapp.com/api";
 
 export async function getHeroes() {
   const url = `/heroes`;
